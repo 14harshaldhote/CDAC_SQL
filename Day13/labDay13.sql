c89f9099360dccb50dbd94a6e97c86b5e71e0187
Harshal Dhote <dhoteh020@gmail.com>
2023-11-01 13:06:17 +0530

    nov1_1

--- Day13/labDay13.sql
+++ Day13/labDay13.sql
@@ -22,4 +22,28 @@ BEGIN
 END//
 delimiter ;
 
+
+use dac_sept23;
 -- 1
+ DELIMITER $$
+
+CREATE PROCEDURE DivideByZeroHandler(IN numerator INT, IN denominator INT)
+BEGIN
+    DECLARE customErrorMessage VARCHAR(255);
+    DECLARE result DECIMAL(10, 2);
+ 
+    IF denominator = 0 THEN
+        select "Error: Division by zero is not allowed." as message;
+    ELSE
+        
+        SET result = numerator / denominator;
+        SELECT result AS Result;
+    END IF;
+END$$
+
+DELIMITER ;
+
+CALL DivideByZeroHandler(10, 5); -- Example with non-zero denominator
+CALL DivideByZeroHandler(10, 0); -- Example with zero denominator
+
+-- 2
